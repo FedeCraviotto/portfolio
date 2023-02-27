b4022ed62b89e4c7260b552964671e9d2c3f7ab7
FedeCraviotto <fcraviotto.dev@gmail.com>
2023-02-27 12:00:48 -0300

    project list separate file

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -1,53 +1,7 @@
 import LineGradient from "../components/LineGradient";
 import { motion } from "framer-motion";
 import Project from "../components/Project";
-
-const projectList = [
-    {
-        title:'Project 1',
-        description:'Analytics Dashboard - React',
-    },
-    {
-        title:'Project 2',
-        description:'Social Network - React/Node',
-    },
-    {
-        title:'Project 3',
-        description:'Estate Agency - React/Django',
-    },
-    {
-        title:'Project 4',
-        description:'Ecommerce - React/Django Braintree',
-    },
-    {
-        title:'Project 5',
-        description:'Ecommerce - React/Node Strapi',
-    },
-    {
-        title:'Project 6',
-        description:'YouTube Clone - React/Node',
-    },
-    {
-        title:'Project 7',
-        description:'Ecommerce - Express/EJS',
-    },
-    {
-        title:'Project 8',
-        description:'Photographer Portfolio - HTML/CSS/Javascript',
-    },
-    {
-        title:'Project 9',
-        description:'Artwork Gallery - Flask CRUD',
-    },
-    {
-        title:'Project 10',
-        description:'RountinApp - Javascript',
-    },
-    {
-        title:'Project 11',
-        description:'Role Game - Javascript CANVAS',
-    },
-]
+import { projectList } from "../utils/projectList";
 
 const container = {
     hidden: {},
